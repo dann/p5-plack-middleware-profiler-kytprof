35db49b5cb7d6e6fe2c31195c0c22d608bd89668
Dann <techmemo@gmail.com>
2013-03-03 22:22:29 +0900

    Add Test::Requires as test dependency

--- lib/Plack/Middleware/Profiler/KYTProf.pm
+++ lib/Plack/Middleware/Profiler/KYTProf.pm
@@ -2,7 +2,7 @@ package Plack::Middleware::Profiler::KYTProf;
 use strict;
 use warnings;
 use parent qw(Plack::Middleware);
-our $VERSION = '0.05';
+our $VERSION = '0.051';
 
 use Plack::Util::Accessor qw(
     namespace_regex
